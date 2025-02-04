5fc33a57618957f385d763a233612933b630a2fc
joaovictortvs <tavaressjoaoo09@gmail.com>
2025-02-04 15:01:28 -0300

    Bug na contagem de pergunta

--- src/components/levelsQuiz/Facil.js
+++ src/components/levelsQuiz/Facil.js
@@ -12,44 +12,66 @@ function Facil(){
     const [removeLoading, setRemoveLoading] = useState(false)
     const [questShow, setQuestShow] = useState(false)
     const [erroQuestShow, setErroQuestShow] = useState(false)
+    const [terminoQuiz, setTerminoQuiz] = useState(false)
 
-    const [titulo, setTitulo] = useState('')
+    const [titulo, setTitulo] = useState()
     const [quests, setQuests] = useState([])
+    const [indiceAtual, setIndiceAtual] = useState(0)
+    const [questsAtual, setQuestsAtual] = useState()
 
     const { id } = useParams()
     const location = useLocation()    
-    
     const level = location.state?.level
 
     function createQuestion(questions){
         setRemoveLoading(true)
         setQuestShow(true)
-        
-        setTitulo(questions[0].question)
         setQuests(questions)
-
+        
+        setIndiceAtual((prevIndice)=>{
+            if(prevIndice < questions.length - 1){
+                return prevIndice + 1
+            } else{
+                setTerminoQuiz(true)
+                return prevIndice
+            }
+        })
 
     }
 
+    useEffect(()=>{
+        if(quests.length > 0 && indiceAtual < quests.length){
+            setTitulo(quests[indiceAtual].question)
+            setQuestsAtual(quests[indiceAtual].answers)
+        }
+    }, [indiceAtual, quests])
 
     useEffect(()=>{
-        fetch(`https://quizapi.io/api/v1/questions?category=${id}&difficulty=${level}&limit=10`, {
+        const options = {
             method: 'GET',
             headers: {
                 'X-Api-Key': "9irGLKfYx2BJQbpKf4dBDCw4k7LKRG6CexZY4zTD",
                 'Content-Type': "application/json"
             }
-        })
+        }
 
-        .then(resp => resp.json())
-        .then((data)=>{
-            createQuestion(data)
-        })
-        .catch((err)=>{
-            console.log(err)
-            setErroQuestShow(true)
-        })
-    }, [])
+        async function requestApi(){
+            try{
+                  const response = await fetch(`https://quizapi.io/api/v1/questions?category=${id}&difficulty=${level}&limit=10`, options)
+
+                const data = await response.json()
+                
+                setQuests(data)
+                createQuestion(data)
+            } catch(err){
+                console.log(err)
+                setErroQuestShow(true)
+            }
+          
+        }
+        requestApi()
+
+    }, [id, level])
 
     return(
         <div className="flex w-full h-full justify-center">
@@ -58,10 +80,12 @@ function Facil(){
                     <img src={Loading} className="w-12 pt-4" alt="loading"/>
                 </div>
             )}
-            {questShow !== false && (
+            {questShow !== false &&  (
                 <div className="flex flex-col w-5/10 text-black items-center flex-wrap">
+                    <p className="text-2xl text-gray-100">Pergunta {indiceAtual} de 10</p>
+                    {terminoQuiz && (<p className="text-green-500 my-2">Quiz terminado!</p>)}
                     <Titulo titulo={titulo}/>
-                    <Perguntas perguntas={quests}/>
+                    <Perguntas perguntas={questsAtual} />
                 </div>
             )}  
             {erroQuestShow && (
--- src/components/quizComponents/Titulo.js
+++ src/components/quizComponents/Titulo.js
@@ -2,7 +2,7 @@ function Titulo({titulo}){
 
     return(
         <div className="h-2/10 mt-2">
-            <p className="max-w-9/10 text-2xl text-amber-100">{titulo}</p>
+            <p className="max-w-9/10 text-2xl text-amber-100 text-center">{titulo}</p>
         </div>
     )
 
